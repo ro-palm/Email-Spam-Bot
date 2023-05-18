52b508b0cfa58ea0c94546b33d7febed3c1f672c
ro-palm <92013821+ro-palm@users.noreply.github.com>
2023-05-18 14:02:09 +0200

    Update spamBot.py

--- spamBot.py
+++ spamBot.py
@@ -1,12 +1,12 @@
 import smtplib
-toaddrs = '578293@student.belgiumcampus.ac.za'
-fromaddrs = 'fyou18314@gmail.com'
-message = 'Zander You Have Been Hacked'
+toaddrs = 'Receiver email'
+fromaddrs = 'Sender Email'
+message = 'Type message here'
 with smtplib.SMTP('smtp.gmail.com', '587') as smtpserver:
   smtpserver.ehlo()
   smtpserver.starttls()
   smtpserver.ehlo()
-  smtpserver.login('fyou18314@gmail.com', 'ihprhlgyopvioomw')
-  for i in range(10):
+  smtpserver.login('Sender email', 'App Password')
+  for i in range(1000):
     smtpserver.sendmail(fromaddrs, toaddrs, message)
     print(i)
